698493b98166ec5ea7387fcf2d6114db5ad0a58d
DmitryMarkov <forall@stylive.com>
2017-04-17 10:49:38 +0300

    Update 2017-04-17T07:49:33.790Z

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+;(function () {
+  'use strict'
+
+  setTimeout(() => {
+    document.querySelector('.greating_picture').classList.add('m--show')
+  }, 1000)
+})()
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIjsoZnVuY3Rpb24gKCkge1xuICAndXNlIHN0cmljdCdcblxyXG4gIHNldFRpbWVvdXQoKCkgPT4ge1xuICAgIGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJy5ncmVhdGluZ19waWN0dXJlJykuY2xhc3NMaXN0LmFkZCgnbS0tc2hvdycpXG4gIH0sIDEwMDApXG59KSgpXG4iXX0=
